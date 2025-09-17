df44eaf25c7e65f161e64daf66ac75b93d50458c
object-Object <object@objectobject.ca>
2025-09-17 15:06:27 -0400

    Add favicon

--- codedeploy/hooks/after-install.sh
+++ codedeploy/hooks/after-install.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euox pipefail
 
-mkdir -p /mnt/blockstorage/codedeploy-apps/object-gay
-
 cd /var/lib/codedeploy-apps/object-gay
 
 docker login ghcr.io --username object-Object --password-stdin < /var/lib/codedeploy-apps/.cr_pat
