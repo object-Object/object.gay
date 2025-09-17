3e2b0d249f06c6a714b767b8b5bf3302c4e390d7
object-Object <object@objectobject.ca>
2025-09-17 14:07:27 -0400

    Move docker pull from application-start to after-install

--- codedeploy/hooks/after-install.sh
+++ codedeploy/hooks/after-install.sh
@@ -2,3 +2,9 @@
 set -euox pipefail
 
 mkdir -p /mnt/blockstorage/codedeploy-apps/object-gay
+
+cd /var/lib/codedeploy-apps/object-gay
+
+docker login ghcr.io --username object-Object --password-stdin < /var/lib/codedeploy-apps/.cr_pat
+
+docker compose pull
--- codedeploy/hooks/application-start.sh
+++ codedeploy/hooks/application-start.sh
@@ -3,8 +3,6 @@ set -euox pipefail
 
 cd /var/lib/codedeploy-apps/object-gay
 
-docker login ghcr.io --username object-Object --password-stdin < /var/lib/codedeploy-apps/.cr_pat
-
 if ! docker compose up --detach --wait --wait-timeout 120 ; then
     docker compose logs
     exit 1
